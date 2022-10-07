33387227c317dc277d33c2f96150661017b94b77
Matheus <matheusdearaujo1209@gmail.com>
2022-10-06 22:48:13 -0300

    pegando dados do Dólar e Euro

---
+++ selenium/app.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+navegador = webdriver.Chrome()
+
+navegador.get("https://www.google.com.br/")
+
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dolar")
+
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacaoDolar = navegador.find_element("xpath", '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+print("Valor do Dólar (em Real):", cotacaoDolar)
+
+#
+
+navegador.get("https://www.google.com.br/")
+
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
+
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacaoEuro = navegador.find_element("xpath", '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+print("Valor do Euro (em Real):", cotacaoEuro)
