e2b506d28ff7f7b8caf6d1a33f274e392b323531
Matheus <matheusdearaujo1209@gmail.com>
2022-10-06 22:53:26 -0300

    pegando dados do Dólar, Euro e Iene

--- selenium/app.py
+++ selenium/app.py
@@ -25,3 +25,15 @@ navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div
 cotacaoEuro = navegador.find_element("xpath", '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 
 print("Valor do Euro (em Real):", cotacaoEuro)
+
+#
+
+navegador.get("https://www.google.com.br/")
+
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação iene")
+
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacaoIene = navegador.find_element("xpath", '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+print("Valor do Euro (em Real):", cotacaoIene)
