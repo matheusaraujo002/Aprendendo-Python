b19da81759ef2d068a285bc8b591c62ea5454e55
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-05-23 22:05:33 -0300

    Introdução na estrutura de repetição

---
+++ Univille/2bimestre/intro-repetições/introrepeticao.py
@@ -0,0 +1,14 @@
+import time
+conta = 0
+i = 1
+while (conta <= 10):
+    time.sleep(0.5)
+    print(conta)
+    conta = conta + i
+
+conta2 = 10
+i = 1
+while (conta2 >= 0):
+    time.sleep(0.5)
+    print(conta2)
+    conta2 = conta2 - i
