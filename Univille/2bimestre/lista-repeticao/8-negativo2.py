0daab586166fd2fbfd9d0c0bccf75bd690cc31cf
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-06-06 22:03:03 -0300

    Segundo programa de negativos

---
+++ Univille/2bimestre/lista-repeticao/8-negativo2.py
@@ -0,0 +1,21 @@
+maiorNum = 0
+menorNum = 0
+contador = 0
+
+while True:
+    num = int(input("Digite o número: "))
+    if num < 0:
+        break
+    
+    if num > maiorNum:
+        maiorNum = num
+    
+    if contador == 0:
+        menorNum = num
+    else:
+        if num < menorNum:
+            menorNum = num
+        contador += 1
+
+print(f"O maior número é {maiorNum}")
+print(f"O menor número é {menorNum}")
