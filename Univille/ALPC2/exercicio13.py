7771103ab4bbc5f5cf7b2a992f0b4129e8277a4a
= <=>
2023-07-31 20:28:34 -0300

    Primeiros exercícios de ALPC2

---
+++ Univille/ALPC2/exercicio13.py
@@ -0,0 +1,20 @@
+A = 10
+B = 20
+
+B = B - A
+A = A + A
+
+print(f'O valor de A é de {A} e o B de {B}')
+
+
+""" OR """
+
+
+""" 
+a = 10
+b = 20
+
+aux = a
+b = a
+b = aux
+"""
---
+++ Univille/ALPC2/tabuada.py
@@ -0,0 +1,4 @@
+tabuada = int(input("Digite o número da tabuada: "))
+
+for i in range(1, 11):
+    print(f"{tabuada} x {i} = {tabuada * i}")
