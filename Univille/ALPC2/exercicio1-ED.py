dac8b6ffd34327e64c959e6b6ea5362997deff6d
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-08-03 16:33:53 -0300

    Maior e menor número

---
+++ Univille/ALPC2/exercicio1-ED.py
@@ -0,0 +1,11 @@
+""" 
+1. Faça um programa que peça dois números e imprima o maior deles.
+"""
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 > num2:
+    print(f"O maior número é {num1}")
+else:
+    print(f"O maior número é {num2}")
