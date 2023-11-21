97fd8f47a4b7fe5e5cff7b41168d3cafcd0ba6af
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-11-20 23:27:16 -0300

    Função com três argumentos para soma-los

---
+++ Univille/ALPC2/Funções/exercicio2.1-F.py
@@ -0,0 +1,8 @@
+""" 
+Crie uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+"""
+
+def somaDeTres(n1, n2, n3):
+    return n1 + n2 + n3
+
+print(somaDeTres(1,2,3))
